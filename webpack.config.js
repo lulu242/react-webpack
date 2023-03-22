9e8f3cc93a2e2a541ba455fe4596e6f512e9fed0
wonyeongeun <kgsol3242@gmail.com>
2023-03-22 13:17:05 +0900

    번들링 완료

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              //preset 여러개이므로 s붙음
+              ['@babel/preset-env'],
+              ['@babel/preset-react', { runtime: 'automatic' }], //빌드하는 동시에 변환시켜 적용해줌(필수적으로 넣어주기)
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+};
